fd312ffb5d8de30acbb20aa6e853f703dd5a779f
rajneesh <rk5677283@gmail.com>
2026-01-13 20:04:12 +0530

    this is second commit

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
-from langchain_community.llms import Ollama
-
+from langchain_ollama import OllamaLLM
 app = FastAPI()
 
-llm = Ollama(model="llama3", base_url="http://localhost:11434")
+llm = OllamaLLM(model="llama3", base_url="http://localhost:11434")
 
-@app.get("/chat")
+@app.get("/")
 def chat(query: str):
      response = llm.invoke(query) 
      return {"response": response}  
